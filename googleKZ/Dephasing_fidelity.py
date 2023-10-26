bf09b985a57b1a4d3f6247c8deca5dd5194408ae
DaRos97 <dario.rossim@gmail.com>
2023-10-26 15:38:40 +0200

    Fidelity now works also for open case, populations still not

--- googleKZ/Dephasing_fidelity.py
+++ googleKZ/Dephasing_fidelity.py
@@ -5,14 +5,14 @@ import dephasing_functions as fs
 import parameters as ps
 #
 #Additional parameters
-list_Tau = [100,200,]
-save_data = False
+list_Tau = [100,400,1000]
+save_data = True
 s_ = 20 #fontsize
-steps = 20
+steps = 50
 
 h_t,J_t,times_dic = ps.find_parameters(list_Tau,steps)
 
-list_gamma = [0,]#np.linspace(0,1,11)
+list_gamma = [0,1e-3,1e-2,1e-1]
 fid_ = []
 for gamma in list_gamma:
     args = (h_t,J_t,gamma, times_dic, list_Tau, ps.homedir, save_data)
@@ -22,7 +22,7 @@ fig = plt.figure(figsize=(16,8))
 X = np.linspace(0,1,steps)
 for i in range(len(list_gamma)):
     for t,Tau in enumerate(list_Tau):
-        plt.plot(X,fid_[i][t],color=fs.cols[t%len(fs.cols)],label=r'$\gamma$='+"{:.4f}".format(list_gamma[i])+r', $\tau$='+str(Tau))
+        plt.plot(X,fid_[i][t],color=fs.cols[i%len(fs.cols)],label=r'$\gamma$='+"{:.4f}".format(list_gamma[i])+r', $\tau$='+str(Tau))
 plt.xlim(-0.02,1.02)
 plt.ylabel(r'$|\langle\psi_{GS}|\psi(t)\rangle|^2$',size=s_)
 plt.xticks([0,1/2,1],['$t_i$','$t_{CP}$','$t_f$'],size=s_)
--- googleKZ/dephasing_functions.py
+++ googleKZ/dephasing_functions.py
@@ -30,7 +30,6 @@ def time_evolve(args):
     h_t,J_t,gamma,times_dic,list_Tau,home_dirname,save_data = args
     result_dirname = home_dirname+dirname_open
     #
-#    rho_ = []
     eta_ = []
     for n,Tau in enumerate(list_Tau):
         dt = times_dic[Tau][1]-times_dic[Tau][0]
@@ -44,14 +43,13 @@ def time_evolve(args):
             eta_.append(eta)
         except:
             print("Time evolution of gamma = ",gamma," ...")
-#            rho = np.zeros((steps,N**2,N),dtype=complex)     #steps->time, N**2 -> number of components of DM, N -> number of modes
+            rho = np.zeros((steps,N**2),dtype=complex)
             eta = np.zeros((steps,N,N),dtype=complex)
             #initial state
             E_0, psi_0 = scipy.linalg.eigh(H_t(N,h_t,J_t,0))
             for m in range(N//2):
-#                rho[0,:,m] = np.reshape(np.outer(psi_0[:,m],psi_0[:,m]),(N**2))
-#                if m < N//2:
-                eta[0] += np.outer(psi_0[:,m],psi_0[:,m])
+                rho[0] += np.reshape(np.outer(psi_0[:,m],psi_0[:,m]),(N**2))
+            eta[0] = np.reshape(rho[0],(N,N))
             #Lindblad part of master equation -> t-independent
             S_L = np.zeros((N**2,N**2),dtype=complex)
             for i in range(N):
@@ -62,10 +60,8 @@ def time_evolve(args):
                 ham = H_t(N,h_t,J_t,s)
                 S_H = -1j*2*np.pi*(L_A(ham)-R_A(ham))
                 exp_S = expm((S_H+S_L)*dt)      #dt is IMPORTANT !!!
-#                for m in range(N):
-#                    rho[s,:,m] = np.matmul(exp_S,rho[s-1,:,m])
-                eta[s] = np.reshape(np.matmul(exp_S,np.reshape(eta[s-1],N**2)),(N,N))
-#            rho_.append(rho)
+                rho[s] = np.matmul(exp_S,rho[s-1])
+                eta[s] = np.reshape(rho[s],(N,N))
             eta_.append(eta)
             #
             if save_data:
@@ -103,11 +99,11 @@ def compute_fidelity(args):
                 E_, psi_gs = np.linalg.eigh(H_t(N,h_t,J_t,s))   #GS at time-step s
                 for m in range(N//2):
                     rho_g += np.outer(psi_gs[:,m],psi_gs[:,m])
-                D_s, M_s = np.linalg.eigh(rho[s])
                 D_g, M_g = np.linalg.eigh(rho_g)
-                overlap_matrix = np.matmul(np.conjugate(M_s[:,N//2:]).T,M_g[:,N//2:])
-                det = np.linalg.det(overlap_matrix)
-                fid_[-1][s] = np.absolute(det)**2
+                rho_tilde = np.matmul(np.matmul(np.conjugate(M_g).T,rho[s]),M_g)
+                nksp = np.real(np.diagonal(rho_tilde)[:N//2])
+                nksm = np.real(np.diagonal(rho_tilde)[N//2:])
+                fid_[-1][s] = np.prod(nksm)
             if save_data:
                 np.save(filename,fid_[-1])
     return fid_
@@ -154,14 +150,24 @@ def compute_populations(args):
                 for k in range(N):
                     res[k] = np.real(G2[:N//2,k].sum())
             else:   #Use Trace
-                res = np.zeros(N)
-                for k in range(N):
-                    n_k = np.zeros((N,N),dtype=complex)
-                    for i in range(N):
-                        for j in range(N):
-                            n_k[i,j] = np.exp(-1j*np.pi*(k)/N*(i-j))
-                    exp_val = np.trace(np.matmul(rho[ind_T],n_k))/N
-                    res[k] = np.absolute(exp_val)
+                #H_fic = np.zeros((N,N),dtype=complex)
+                #for i in range(N):
+                #    H_fic[i,i] = (-1)**i
+                H_fic = H_t(N,h_t,np.zeros((N,N)),0)
+                D_g, M_g = np.linalg.eigh(H_fic)
+                res = np.real(np.diagonal(np.matmul(np.matmul(np.conjugate(M_g).T,rho[ind_T]),M_g)))
+                
+                #res = np.zeros(N)
+                #for k in range(N):
+                #    n_k = np.zeros((N,N),dtype=complex)
+                #    for i in range(N):
+                #        for j in range(N):
+                #            n_k[i,j] = np.exp(-1j*np.pi*(k)/N*(i-j))
+                #    n_k_tilde = np.matmul(np.matmul(np.conjugate(M_t).T,n_k),M_t)
+                #    print(k,np.sum(np.diagonal(n_k_tilde))/N)
+                #exit()
+                    #exp_val = np.trace(np.matmul(rho[ind_T],n_k))/N
+                    #res[k] = np.absolute(exp_val)
             #
             n_q.append(res)
             #
