9f3515bd35b1f1d6d77aba4a0870fa9300b62e42
DaRos97 <dario.rossim@gmail.com>
2025-01-28 13:40:13 +0100

    removed data from sync new_1D

--- new_1D/README.md
+++ new_1D/README.md
@@ -4,8 +4,8 @@
 
 We want to measure the ZZ correlator on a certain state psi. 
 Measured quantity is the Fourier transform of `<Z_i(t)Z_0(0) - Z_0(0)Z_i(t)>`.
-We have PBC, 42 sites and time bw 0 and 900 ns.
-Still unknown how they get the state psi (ramp or exact).
+We have PBC, 42 sites and measuring times between 0 and 800 ns.
+States are obtained through a linear ramp of total time 500 ns starting from the staggered state and arriving to the gappless state.
 Model is still XX+YY + staggered Z.
 
 We compute also the ramp to get to the final measured state by time evolving.
--- new_1D/main.py
+++ new_1D/main.py
@@ -1,18 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.cm import ScalarMappable
+from matplotlib.colors import Normalize
 from pathlib import Path
 from tqdm import tqdm
 from scipy.linalg import expm
 import scipy
 
-plot_fidelity = 0
+plot_fidelity = 1
 plot_populations = 1
+use_time_evolved = 1
 
 g_ = 10 #MHz
 h_ = 15 #MHz
 full_time_ramp = 0.5 #ramp time in ms
 full_time_measure = 0.8     #measure time in ms
-time_step = 0.001    #time step of ramp
+time_step = full_time_ramp/1000
+#time_step = 0.001    #time step of ramp
 
 N = 42  #sites
 Nt = 2000 #time steps after ramp
@@ -43,25 +48,33 @@ for i in range(time_steps):
 
 time_evolved_psi_fn = "data/time_evolved_psi_"+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.npy'
 fidelity_fn = "data/fidelity_"+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.npy'
+pop_fn = "data/populations_"+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.npy'
 if not Path(time_evolved_psi_fn).is_file(): #Time evolution and fidelity along the ramp
-    psi = np.zeros((time_steps,N,N),dtype=complex)    #wavefunction at all time steps of the ramp
+    time_evolved_psi = np.zeros((time_steps,N,N),dtype=complex)    #wavefunction at all time steps of the ramp
     fidelity = np.zeros(time_steps)
-    for i in range(time_steps):
+    pop = np.zeros((time_steps,N))
+    print("Time evolution")
+    for i in tqdm(range(time_steps)):
         if i==0:
-            psi[i] = np.linalg.eigh(compute_H(g[i],h[i],N))[1]
+            time_evolved_psi[i] = np.linalg.eigh(compute_H(g[i],h[i],N))[1]
         else:
             exp_H = expm(-1j*2*np.pi*compute_H(g[i],h[i],N)*time_step)
             for m in range(N):  #evolve each mode independently
-                psi[i,:,m] = exp_H @ psi[i-1,:,m]
+                time_evolved_psi[i,:,m] = exp_H @ time_evolved_psi[i-1,:,m]
         #Fidelity wrt real GS
         E_GS,psi_GS = scipy.linalg.eigh(compute_H(g[i],h[i],N))
-        fidelity[i] = np.absolute(np.linalg.det(psi[i,:,:N//2].T.conj()@psi_GS[:,:N//2]))**2
+        fidelity[i] = np.absolute(np.linalg.det(time_evolved_psi[i,:,:N//2].T.conj()@psi_GS[:,:N//2]))**2
+        #Occupation of populations
+        for k in range(N):
+            pop[i,k] = np.sum((np.absolute(time_evolved_psi[i,:,:].T.conj() @ psi_GS[:,k])**2)[:N//2])
     #Save
-    np.save(time_evolved_psi_fn,psi)
+    np.save(time_evolved_psi_fn,time_evolved_psi)
     np.save(fidelity_fn,fidelity)
+    np.save(pop_fn,pop)
 else:
     time_evolved_psi = np.load(time_evolved_psi_fn)
     fidelity = np.load(fidelity_fn)
+    pop = np.load(pop_fn)
 
 if plot_fidelity:   #Plot fidelity along the ramp and quenched values of h and g
     fig = plt.figure()
@@ -78,43 +91,54 @@ if plot_fidelity:   #Plot fidelity along the ramp and quenched values of h and g
     labels = [l.get_label() for l in ls]
     ax.legend(ls,labels)
     plt.savefig("figures/ramp_fidelity_"+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.png')
-    plt.show()
-    exit()
+#    plt.show()
 #
 if plot_populations:
-    pop = np.zeros((time_steps,N))
-    H = compute_H(g[-1],h[-1],N)
-    evals,evecs = np.linalg.eigh(H)
-    for i_t in range(time_steps):
-        for k in range(N):
-            pop[i,k] = np.sum(time_evolved_psi[i_t,:,k]@time_evolved_psi[i_t].conj())
+    E_GS,psi_GS = scipy.linalg.eigh(compute_H(g[-1],h[1],N))
+    cmap = mpl.colormaps['plasma']
+    colors = cmap(np.linspace(0,1,N//2-1))
     fig = plt.figure()
     ax = fig.add_subplot()
-    for i in range(N//2):
-        ax.plot(np.linspace(0,ramp_time,time_steps),pop[:,i],color='r')
-    plt.show()
-    exit()
+    for i in range(N//2-1):
+        ax.plot(np.linspace(0,ramp_time,time_steps),pop[:,i],color=colors[i])
+    ax.set_ylabel('Modes occupations')
+    ax.set_xlabel('time (ms)')
+    norm = norm = Normalize(vmin=E_GS[0], vmax=E_GS[N//2-1])
+    sm = ScalarMappable(cmap=cmap, norm=norm)  # Create a ScalarMappable for the colorbar
+    sm.set_array([])  # The ScalarMappable needs an array, even if unused
+    cbar = plt.colorbar(sm, ax=ax)
+    cbar.set_label("Energy of modes")
+    plt.savefig("figures/ramp_populations_"+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.png')
+#    plt.show()
 
-fig = plt.figure(figsize=(20, 15))
-for i_sr in range(len(stop_ratio_list)):
+fig = plt.figure(figsize=(20, 12))
+txt_title = 'time evolved wavefunction' if use_time_evolved else 'ground state wavefunction'
+plt.suptitle("Total ramp time: "+str(int(full_time_ramp*1000))+" ns, "+txt_title)
+print("Correlation functions")
+for i_sr in tqdm(range(len(stop_ratio_list))):
     stop_ratio = stop_ratio_list[i_sr]
-    g = g_*stop_ratio
-    h = h_*(1-stop_ratio)
-    H = compute_H(g,h,N)
-    evals,evecs = np.linalg.eigh(H)
-    #Time evolution
-    U_1 = [np.exp(1j*2*np.pi*evals[:N//2]*time) for time in time_list]
-    U_2 = [np.exp(-1j*2*np.pi*evals[N//2:]*time) for time in time_list]
+    i_t = int(time_steps*stop_ratio)-1
+    H = compute_H(g[i_t],h[i_t],N)
+    E_GS,psi_GS = np.linalg.eigh(H)
+    evecs = psi_GS
+    if use_time_evolved:
+        pop_k = pop[i_t]
+    else:
+        pop_k = np.ones(N)
+        pop_k[N//2:] *= 0
+    #Time evolution measurement
+    U_1 = [np.exp(1j*2*np.pi*E_GS[:]*time) for time in time_list]
+    U_2 = [np.exp(-1j*2*np.pi*E_GS[:]*time) for time in time_list]
     #
-    rho1 = np.array([np.einsum('ik,kj,k->ij',evecs[:,:N//2],evecs[:,:N//2].T.conj(),U_1[i_t],optimize=True) for i_t in range(Nt)])
-    rho2 = np.array([np.einsum('ik,kj,k->ij',evecs[:,N//2:],evecs[:,N//2:].T.conj(),U_2[i_t],optimize=True) for i_t in range(Nt)])
+    rho1 = np.array([np.einsum('ik,kj,k,k->ij',evecs[:,:],evecs[:,:].T.conj(),U_1[i_t],pop_k,optimize=True) for i_t in range(Nt)])
+    rho2 = np.array([np.einsum('ik,kj,k,k->ij',evecs[:,:],evecs[:,:].T.conj(),U_2[i_t],1-pop_k,optimize=True) for i_t in range(Nt)])
     #Correlator in space-time
     ZZ = np.zeros((N,Nt),dtype=complex)
     for i in range(N):
         ZZ[i] = np.array([2*1j*np.imag(rho1[i_t,i,0]*rho2[i_t,i,0]) for i_t in range(Nt)])
     #Fourier transform -> momentum-frequency
     ZZ_ft = np.fft.fftshift(np.fft.fft2(ZZ)) / np.sqrt(N*Nt)
-    kx = np.fft.fftshift(np.fft.fftfreq(N, d=1)) * np.pi
+    kx = np.fft.fftshift(np.fft.fftfreq(N, d=1)) * 2 * np.pi
     omega = np.fft.fftshift(np.fft.fftfreq(Nt, d=full_time_measure / Nt))
     #Plot
     ax = fig.add_subplot(2,5,i_sr+1)
@@ -130,9 +154,11 @@ for i_sr in range(len(stop_ratio_list)):
     if i_sr in [0,5]:
         ax.set_ylabel('Frequency $\omega$ (MHz)')
     ax.set_title('Stop ratio '+"{:.1f}".format(stop_ratio))
-#    plt.savefig("figures/1D_GS_100_"+"{:.1f}".format(stop_ratio)+".png")
+
 fig.tight_layout()
-plt.show()
+txt_wf = 'ramp_WF' if use_time_evolved else 'GS_WF'
+plt.savefig("figures/ZZ_FFT_"+txt_wf+'_'+str(N)+'_'+"{:.2f}".format(full_time_ramp)+'.png')
+#plt.show()
 #    exit()
 
 
