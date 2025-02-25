5949e216bad67dfa4b49073e4788c188d15102b1
DaRos97 <dario.rossim@gmail.com>
2025-02-25 11:56:24 +0100

    New name and Readme

--- new_1D/correlators.py
+++ new_1D/correlators.py
@@ -6,26 +6,34 @@ from matplotlib.cm import ScalarMappable
 from matplotlib.colors import Normalize
 from pathlib import Path
 import sys
+import scipy
 
 save_time_evolved_data = True
-plot_fidelity = False
-plot_populations = False
-savefig_fidelity = False
-savefig_populations = False
+plot_fidelity = 0#True
+plot_populations = 0#True
+savefig_fidelity = 0#True
+savefig_populations = 0#True
 
 use_time_evolved = True
-save_zz_correlator_data = True
-plot_zz_correlator = True
-savefig_zz_correlator = False
+#which correlator
+correlator_type = 'zz' if len(sys.argv)<2 else sys.argv[1]
+for i in correlator_type:
+    if i not in ['z','e','j']:  #supported operators
+        print("Not supported operator ",i,"\nAbort")
+#correlator options
+save_correlator_data = True
+plot_correlator = True
+savefig_correlator = True
 
 #Parameters
+N = 42          #chain sites
 g_ = 10 #MHz
 h_ = 15 #MHz
-full_time_ramp = 0.5 if len(sys.argv)<2 else int(sys.argv[1])/1000#ramp time in ms
-full_time_measure = 0.8     #measure time in ms
-time_step = full_time_ramp/500  #time step of ramp
+full_time_ramp = 0.5 if len(sys.argv)<3 else float(sys.argv[2])/1000#ramp time in ms
+time_steps = 500        #of ramp
+time_step = full_time_ramp/time_steps  #time step of ramp
 
-N = 42          #chain sites
+full_time_measure = 0.8     #measure time in ms
 Nt = 401        #time steps after ramp for the measurement
 Nomega = 2000   #Number of frequency points in the Fourier transform
 measure_time_list = np.linspace(0,full_time_measure,Nt)
@@ -35,10 +43,10 @@ stop_ratio_list = np.linspace(0.1,1,10)     #ratios of ramp where we stop and me
 print("Parameters of ramp: ")
 print("Sites: ",N,"\nRamp time (ns): ",full_time_ramp*1000,"\nRamp time step (ns): ",time_step*1000)
 
-g_t,h_t = fs.get_Hamiltonian_parameters(int(full_time_ramp//time_step),g_,h_)   #parameters of Hamiltonian which depend on time
+g_t,h_t = fs.get_Hamiltonian_parameters(time_steps,g_,h_)   #parameters of Hamiltonian which depend on time
 
 """Time evolution"""
-args_fn = [(N,0),(full_time_ramp,3),(time_step,5)]
+args_fn = [(N,0),(full_time_ramp,5),(time_step,5)]
 time_evolved_psi_fn = fs.get_data_filename("time_evolved_psi",args_fn,'.npy')
 fidelity_fn = fs.get_data_filename("fidelity",args_fn,'.npy')
 populations_fn = fs.get_data_filename("populations",args_fn,'.npy')
@@ -53,34 +61,38 @@ else:
     fidelity = np.load(fidelity_fn)
     populations = np.load(populations_fn)
 
-"""ZZ correlator"""
+"""Correlator"""
 txt_wf = 'time-evolved' if use_time_evolved else 'GS-wf'
-args_corr_fn = args_fn + [(txt_wf,0),(full_time_measure,3),(Nt,0),(Nomega,0)]
-zz_correlator_fn = fs.get_data_filename('zz_correlator',args_corr_fn,'.npy')
-if not Path(zz_correlator_fn).is_file():
-    print("Computing correlation function with "+txt_wf+': ')
-    zz_correlator = fs.compute_zz_correlator(*(N,omega_list,stop_ratio_list,measure_time_list,g_t,h_t,time_evolved_psi,use_time_evolved))
-    if save_zz_correlator_data:
-        np.save(zz_correlator_fn,zz_correlator)
+args_corr_fn = args_fn + [(correlator_type,0),(txt_wf,0),(full_time_measure,3),(Nt,0),(Nomega,0)]
+correlator_fn = fs.get_data_filename('correlator',args_corr_fn,'.npy')
+if not Path(correlator_fn).is_file():
+    print("Computing correlation function ",correlator_type," with "+txt_wf+': ')
+    correlator = fs.compute_correlator(correlator_type,*(N,omega_list,stop_ratio_list,measure_time_list,g_t,h_t,time_evolved_psi,use_time_evolved))
+    if save_correlator_data:
+        np.save(correlator_fn,correlator)
 else:
-    zz_correlator = np.load(zz_correlator_fn)
+    correlator = np.load(correlator_fn)
 
 #########################################################################################
 #########################################################################################
 """Plots"""
 
-if plot_zz_correlator:
+if plot_correlator:
     #Plot
     fig = plt.figure(figsize=(17, 8))
     txt_title = 'time evolved wavefunction' if use_time_evolved else 'ground state wavefunction'
-    plt.suptitle("Total ramp time: "+str(int(full_time_ramp*1000))+" ns, "+txt_title)
+    plt.suptitle("Correlator "+correlator_type+", total ramp time: "+str(int(full_time_ramp*1000))+" ns, "+txt_title)
     kx = np.fft.fftshift(np.fft.fftfreq(N,d=1))
     for i_sr in range(len(stop_ratio_list)):
         stop_ratio = stop_ratio_list[i_sr]
         #Plot
         ax = fig.add_subplot(2,5,i_sr+1)
-    #    ax = fig.add_subplot()
-        pm = ax.pcolormesh(kx, omega_list, np.abs(zz_correlator[i_sr]).T, shading='auto', cmap='magma')
+        if 1:
+            pm = ax.pcolormesh(kx, omega_list, (np.abs(correlator[i_sr]).T)**(1), shading='auto', cmap='magma')
+        else:
+            io = 5
+            pm = ax.pcolormesh(kx, omega_list[Nomega//2+io:], np.abs(correlator[i_sr,:,Nomega//2+io:]).T, shading='auto', cmap='magma')
+            pm2 = ax.pcolormesh(kx, omega_list[:Nomega//2-io], np.abs(correlator[i_sr,:,:Nomega//2-io]).T, shading='auto', cmap='magma')
         if i_sr in [4,9]:
             label_cm = 'Magnitude of Fourier Transform'
         else:
@@ -92,56 +104,63 @@ if plot_zz_correlator:
         if i_sr in [0,5]:
             ax.set_ylabel('Frequency $\omega$ (MHz)')
         ax.set_title('Stop ratio '+"{:.1f}".format(stop_ratio))
-        if 0: #plot fit
+        if 0 and correlator_type=='zz': #plot fit
+            time_steps = len(g_t)
             i_t = int(time_steps*stop_ratio)-1
-            en_m = np.sqrt(h_t[i_t]**2+4*g_t[i_t]**2*np.cos(kx*2*np.pi+np.pi/2)**2)+abs(h[i_t])
+            en_m = np.sqrt(h_t[i_t]**2+4*g_t[i_t]**2*np.cos(kx*2*np.pi+np.pi/2)**2)+abs(h_t[i_t])
             en_M = 2*np.sqrt(h_t[i_t]**2+4*g_t[i_t]**2*np.cos(kx*np.pi+np.pi/2)**2)
-            en_ex = en_m-2*abs(h[i_t])
-            ax.plot(kx,en_m,color='r')
+            en_ex = en_m-2*abs(h_t[i_t])
+#            ax.plot(kx,en_m,color='r')
             ax.plot(kx,-en_m,color='r')
-            ax.plot(kx,en_M,color='g')
+#            ax.plot(kx,en_M,color='g')
             ax.plot(kx,-en_M,color='g')
-            ax.plot(kx,en_ex,color='b')
+#            ax.plot(kx,en_ex,color='b')
+            ax.plot(kx,-en_ex,color='b')
     fig.tight_layout()
-    if savefig_zz_correlator:
-        plt.savefig("figures/"+corr_fn[5:-4]+'.png')
+    if savefig_correlator:
+        correlator_figfn = fs.get_data_filename("correlator",args_corr_fn,'.png')
+        plt.savefig(correlator_figfn)
 
 if plot_fidelity:   #Plot fidelity along the ramp and quenched values of h and g
+    time_steps = len(g_t)
     fig = plt.figure()
     ax = fig.add_subplot()
-    l1 = ax.plot(np.linspace(0,ramp_time,time_steps),fidelity,color='r',label="fidelity")
+    l1 = ax.plot(np.linspace(0,full_time_ramp*1000,time_steps),fidelity,color='r',label="fidelity")
     ax.set_ylabel("Fidelity wrt GS")
-    ax.set_xlabel("time (ms)")
+    ax.set_xlabel("time (ns)")
     ax_r = ax.twinx()
-    l2 = ax_r.plot(np.linspace(0,ramp_time,time_steps),g,color='g',label="g")
-    l3 = ax_r.plot(np.linspace(0,ramp_time,time_steps),h,color='b',label="h")
+    l2 = ax_r.plot(np.linspace(0,full_time_ramp*1000,time_steps),g_t,color='g',label="g")
+    l3 = ax_r.plot(np.linspace(0,full_time_ramp*1000,time_steps),h_t,color='b',label="h")
     ax_r.set_ylabel("coupling g and field h (MHz)")
     ax.set_title("Full ramp fidelity")
     ls = l1+l2+l3
     labels = [l.get_label() for l in ls]
     ax.legend(ls,labels)
     if savefig_fidelity:
-        plt.savefig("figures/ramp_fidelity_"+str(N)+'_'+"{:.3f}".format(full_time_ramp)+'.png')
+        fidelity_figfn = fs.get_data_filename("fidelity",args_fn,'.png')
+        plt.savefig(fidelity_figfn)
 
 if plot_populations:
-    E_GS,psi_GS = scipy.linalg.eigh(compute_H(g[-1],h[1],N))
+    time_steps = len(g_t)
+    E_GS,psi_GS = scipy.linalg.eigh(fs.compute_H(g_t[-1],h_t[-1],N,1))
     cmap = mpl.colormaps['plasma']
-    colors = cmap(np.linspace(0,1,N))#N//2-1))
+    colors = cmap(np.linspace(0,1,N//2))
     fig = plt.figure()
     ax = fig.add_subplot()
     for i in range(N//2,N):#N//2-1):
-        ax.plot(np.linspace(0,ramp_time,time_steps),populations[:,i],color=colors[i-N//2])
+        ax.plot(np.linspace(0,full_time_ramp*1000,time_steps),populations[:,i],color=colors[i-N//2])
     ax.set_ylabel('Modes occupations')
-    ax.set_xlabel('time (ms)')
-    norm = Normalize(vmin=E_GS[N//2], vmax=E_GS[N-1])
+    ax.set_xlabel('time (ns)')
+    norm = Normalize(vmin=E_GS[N//2], vmax=E_GS[-1])
     sm = ScalarMappable(cmap=cmap, norm=norm)  # Create a ScalarMappable for the colorbar
     sm.set_array([])  # The ScalarMappable needs an array, even if unused
     cbar = plt.colorbar(sm, ax=ax)
     cbar.set_label("Energy of modes")
     if savefig_populations:
-        plt.savefig("figures/"+pop_fn[5:-4]+'.png')
+        populations_figfn = fs.get_data_filename("populations",args_fn,'.png')
+        plt.savefig(populations_figfn)
 
-if plot_fidelity or plot_populations or plot_zz_correlator:
+if plot_fidelity or plot_populations or plot_correlator:
     plt.show()
 
 
--- new_1D/functions.py
+++ new_1D/functions.py
@@ -14,59 +14,101 @@ def get_ramp_evolution(*args):
     print("Time evolution")
     for it in tqdm(range(time_steps)):
         if it==0:
-            time_evolved_psi[it] = np.linalg.eigh(compute_H(g_t[it],h_t[it],N))[1]      #exact GS at t=0
+            time_evolved_psi[it] = scipy.linalg.eigh(compute_H(g_t[it],h_t[it],N,1))[1]      #exact GS at t=0
         else:
-            exp_H = expm(-1j*2*np.pi*compute_H(g_t[it],h_t[it],N)*time_step)
+            exp_H = expm(-1j*2*np.pi*compute_H(g_t[it],h_t[it],N,1)*time_step)
+            #exp_H = expm(-1j*compute_H(g_t[it],h_t[it],N,1)*time_step)
             for m in range(N):  #evolve each mode independently
                 time_evolved_psi[it,:,m] = exp_H @ time_evolved_psi[it-1,:,m]
         #Fidelity wrt real GS
-        E_GS,psi_GS = scipy.linalg.eigh(compute_H(g_t[it],h_t[it],N))
-        fidelity[it] = np.absolute(np.linalg.det(time_evolved_psi[it,:,:N//2].T.conj()@psi_GS[:,:N//2]))**2
+        E_GS,psi_GS = scipy.linalg.eigh(compute_H(g_t[it],h_t[it],N,1))
+        fidelity[it] = np.absolute(scipy.linalg.det(time_evolved_psi[it,:,:N//2].T.conj()@psi_GS[:,:N//2]))**2
         #Occupation of populations
         populations[it] = compute_populations(time_evolved_psi[it],psi_GS)
     return time_evolved_psi, fidelity,populations
 
-def compute_zz_correlator(*args):
-    """Compute the ZZ correlator and its Fourier transform."""
+def compute_H(g_nn,h_field,N_sites,P=1):
+    """NxN fermion Hamiltonian with PBC"""
+    ham = np.zeros((N_sites,N_sites))
+    h_list = np.ones(N_sites)*h_field
+    for i in range(N_sites-1):
+        ham[i,i+1] = g_nn
+        ham[i+1,i] = g_nn
+        ham[i,i] = h_list[i]*(-1)**(i+1)
+    ham[-1,-1] = h_list[-1]*(-1)**(N_sites)
+    #PBC
+    ham[-1,0] = P*g_nn    #*P fermion number
+    ham[0,-1] = P*g_nn
+    return ham
+
+def compute_correlator(correlator_type,*args):
+    """Compute the correlator and its Fourier transform."""
     N,omega_list,stop_ratio_list,measure_time_list,g_t,h_t,time_evolved_psi,use_time_evolved = args
     Nt = len(measure_time_list)
     Nomega = len(omega_list)
     time_steps = len(g_t)
     #
-    zz_correlator = np.zeros((len(stop_ratio_list),N,Nomega),dtype=complex)
+    corr_kw = np.zeros((len(stop_ratio_list),N,Nomega),dtype=complex)
     for i_sr in tqdm(range(len(stop_ratio_list))):
         stop_ratio = stop_ratio_list[i_sr]
         i_t = int(time_steps*stop_ratio)-1
-        E_GS,beta = scipy.linalg.eigh( compute_H(g_t[i_t],h_t[i_t],N) )
-#        _,beta = np.linalg.eigh(compute_H(g[-1],h[-1],N))
+        E_GS,beta = scipy.linalg.eigh( compute_H(g_t[i_t],h_t[i_t],N,1) )
         if use_time_evolved:
             alpha = np.copy(time_evolved_psi[i_t][:,:N//2])
         else:
             alpha = np.copy(beta[:,:N//2])
         #Time evolution and correlators
         U_t = [np.exp(-1j*2*np.pi*E_GS*time) for time in measure_time_list]
-        G_ij = get_Gij(alpha,beta,U_t)
-        H_ij = get_Hij(alpha,beta,U_t)
-        #Correlator in space-time
-        ZZ = np.zeros((N,Nt),dtype=complex)
+        G_ij = get_Gij(alpha,beta,U_t)/np.sqrt(N/2)
+        H_ij = get_Hij(alpha,beta,U_t)/np.sqrt(N/2)
+        #Correlator in space-time: here we decide which correlator we are computing
+        corr_xt = np.zeros((N,Nt),dtype=complex)
+        func_corr = dic_correlators[correlator_type]
         for i in range(N):
-            ZZ[i] = np.array([2*1j*np.imag(G_ij[i_t,i,0]*H_ij[i_t,i,0]) for i_t in range(Nt)])/N*2
+            corr_xt[i] = np.array([func_corr(G_ij[i_t],H_ij[i_t],i) for i_t in range(Nt)])
         #2D FFT
-        zz_correlator[i_sr] = np.fft.fftshift(np.fft.fft2(ZZ,[N,Nomega]))
-    return zz_correlator
+        corr_kw[i_sr] = np.fft.fftshift(np.fft.fft2(corr_xt,[N,Nomega]))
+    return corr_kw
 
-def compute_H(g_nn,h_field,N_sites):
-    """NxN fermion Hamiltonian with PBC"""
-    ham = np.zeros((N_sites,N_sites))
-    for i in range(N_sites-1):
-        ham[i,i+1] = g_nn
-        ham[i+1,i] = g_nn
-        ham[i,i] = h_field*(-1)**(i+1)
-    ham[-1,-1] = h_field*(-1)**(N_sites)
-    #PBC
-    ham[-1,0] = -g_nn
-    ham[0,-1] = -g_nn
-    return ham
+def correlator_zz(G,H,i):
+    ip1 = (i+1)%G.shape[0]  #i+1 is modulo N -> N+1=1
+    return 2*1j*np.imag(G[i,0]*H[i,0])
+def correlator_ee(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return 2*1j*np.imag(G[i,1]*H[ip1,0]+G[i,0]*H[ip1,1]+G[ip1,1]*H[i,0]+G[ip1,0]*H[i,1])
+def correlator_jj(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return -2*1j*np.imag(G[i,1]*H[ip1,0]-G[i,0]*H[ip1,1]-G[ip1,1]*H[i,0]+G[ip1,0]*H[i,1])
+def correlator_ez(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return 2*1j*np.imag(G[i,0]*H[ip1,0]+G[ip1,0]*H[i,0])
+def correlator_ze(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return 2*1j*np.imag(G[i,1]*H[i,0]+G[i,0]*H[i,1])
+def correlator_jz(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return -2*np.imag(G[i,0]*H[ip1,0]-G[ip1,0]*H[i,0])
+def correlator_zj(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return -2*np.imag(G[i,1]*H[i,0]-G[i,0]*H[i,1])
+def correlator_ej(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return -2*np.imag(G[i,1]*H[ip1,0]-G[i,0]*H[ip1,1]+G[ip1,1]*H[i,0]-G[ip1,0]*H[i,1])
+def correlator_je(G,H,i):
+    ip1 = (i+1)%G.shape[0]
+    return -2*np.imag(G[i,1]*H[ip1,0]+G[i,0]*H[ip1,1]-G[ip1,1]*H[i,0]-G[ip1,0]*H[i,1])
+
+dic_correlators = {
+    'zz':correlator_zz,
+    'ee':correlator_ee,
+    'jj':correlator_jj,
+    'ez':correlator_ez,
+    'ze':correlator_ze,
+    'jz':correlator_jz,
+    'zj':correlator_zj,
+    'ej':correlator_ej,
+    'je':correlator_je,
+}
 
 def compute_populations(psi_t,psi_GS):
     """Compute the populations of the single modes"""
@@ -87,12 +129,9 @@ def get_Hij(alpha,beta,U):
 
 def get_Hamiltonian_parameters(time_steps,g_,h_):
     """Compute g(t) and h(t) for each time of the ramp."""
-    g = np.zeros(time_steps)
-    h = np.zeros(time_steps)
-    for i in range(time_steps):
-        g[i] = 0 + g_/(time_steps-1)*i
-        h[i] = h_ - h_/(time_steps-1)*i
-    return g,h
+    g_t = np.linspace(0,10,time_steps)
+    h_t = np.linspace(15,0,time_steps)
+    return g_t,h_t
 
 def get_data_filename(spec_name,pars,extension):
     """Define the filename for data. Pars is a list of tuple, each tuple has the argument and the number of decimals precision."""
